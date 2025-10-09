ce9ae4b861c683f730d1e4a48fafe8ff76b21a79
riessz <arthurriess10@gmail.com>
2025-10-09 17:20:37 -0300

    update models classes

--- src/intro/myapp/models.py
+++ src/intro/myapp/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
-class Produto(models.Model):
+class Aluno(models.Model):
+    matricula = models.CharField(max_length=20, unique=True)
     nome = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=6, decimal_places=2)
-    descricao = models.TextField()
-    disponivel = models.BooleanField(default=True)
+    cpf = models.CharField(max_length=14, unique=True)
+    crgeral = models.FloatField()
+    curso = models.CharField(max_length=100)
+    disciplina = models.CharField(max_length=100)
+    periodo = models.IntegerField()
+    telefone = models.CharField(max_length=15)
 
+class Professor(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14, unique=True)
+    telefone = models.CharField(max_length=15)
+
+class Vaga(models.Model):
+    nome = models.CharField(max_length=100)
+    crminimo = models.FloatField()
+    disciplina = models.CharField(max_length=100)
+    statusvaga = models.CharField(max_length=20)
+   
     def __str__(self):
         return self.nomepython 
